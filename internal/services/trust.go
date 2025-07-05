ad781c6c11b32718f0a5e8d90fbf8a77f47819d2
Firas Ghanmi <fghanmi@redhat.com>
2025-07-05 15:59:21 +0200

    add targets,target, targets/certificates routes

--- internal/services/trust.go
+++ internal/services/trust.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"context"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"io"
 	"net/http"
@@ -24,6 +26,9 @@ import (
 type TrustService interface {
 	GetTrustConfig(ctx context.Context, tufRepoUrl string) (models.TrustConfig, error)
 	GetTrustRootMetadataInfo(tufRepoUrl string) (models.RootMetadataInfoList, error)
+	GetTargetsList(ctx context.Context, tufRepoUrl string) (models.TargetsList, error)
+	GetTarget(ctx context.Context, tufRepoUrl string, target string) (models.TargetContent, error)
+	GetCertificatesInfo(ctx context.Context, tufRepoUrl string) (models.CertificateInfoList, error)
 }
 
 type trustService struct{}
@@ -59,15 +64,118 @@ func (s *trustService) GetTrustRootMetadataInfo(tufRepoUrl string) (models.RootM
 		if err != nil {
 			return models.RootMetadataInfoList{}, fmt.Errorf("extracting root metadata info: %w", err)
 		}
-		entry := models.RootMetadataInfo{
-			Version: rootInfo["version"],
-			Expires: rootInfo["expires"],
-			Status:  rootInfo["status"],
-		}
-		results.Data = append(results.Data, entry)
+		results.Data = append(results.Data, rootInfo)
 	}
 
 	return results, nil
+}
+
+func (s *trustService) GetTargetsList(ctx context.Context, tufRepoUrl string) (models.TargetsList, error) {
+	opts := buildTufOptions(tufRepoUrl)
+	tufCfg, err := buildTufConfig(opts)
+	if err != nil {
+		return models.TargetsList{}, err
+	}
+
+	up, err := updater.New(tufCfg)
+	if err != nil {
+		return models.TargetsList{}, fmt.Errorf("failed to create updater: %w", err)
+	}
+
+	if err := up.Refresh(); err != nil {
+		return models.TargetsList{}, fmt.Errorf("failed to refresh updater: %w", err)
+	}
+
+	// Get Targets list
+	targetFiles := up.GetTopLevelTargets()
+	var targetList []string
+	for target := range targetFiles {
+		targetList = append(targetList, target)
+	}
+
+	return models.TargetsList{Targets: targetList}, nil
+}
+
+func (s *trustService) GetTarget(ctx context.Context, tufRepoUrl string, target string) (models.TargetContent, error) {
+	opts := buildTufOptions(tufRepoUrl)
+	tufCfg, err := buildTufConfig(opts)
+	if err != nil {
+		return models.TargetContent{}, err
+	}
+
+	up, err := updater.New(tufCfg)
+	if err != nil {
+		return models.TargetContent{}, fmt.Errorf("failed to create updater: %w", err)
+	}
+
+	if err := up.Refresh(); err != nil {
+		return models.TargetContent{}, fmt.Errorf("failed to refresh updater: %w", err)
+	}
+
+	// Get Target content
+	const filePath = ""
+	ti, err := up.GetTargetInfo(target)
+	if err != nil {
+		return models.TargetContent{}, fmt.Errorf("getting info for target \"%s\": %w", target, err)
+	}
+	path, tb, err := up.FindCachedTarget(ti, filePath)
+	if err != nil {
+		return models.TargetContent{}, fmt.Errorf("getting target cache: %w", err)
+	}
+	if path != "" {
+		// Cached version found
+		return models.TargetContent{Content: string(tb)}, nil
+	}
+
+	// Download of target is needed
+	// Ignore targetsBaseURL, set to empty string
+	const targetsBaseURL = ""
+	_, tb, err = up.DownloadTarget(ti, filePath, targetsBaseURL)
+	if err != nil {
+		return models.TargetContent{}, fmt.Errorf("failed to download target file %s - %w", target, err)
+	}
+
+	return models.TargetContent{Content: string(tb)}, nil
+}
+
+func (s *trustService) GetCertificatesInfo(ctx context.Context, tufRepoUrl string) (models.CertificateInfoList, error) {
+	opts := buildTufOptions(tufRepoUrl)
+	targetsBytes, err := fetchTufTargetsMetadata(opts)
+	if err != nil {
+		return models.CertificateInfoList{}, fmt.Errorf("fetching TUF target metadata: %w", err)
+	}
+	targetMetadataSpecs, err := extractTargetMetadataInfo(targetsBytes)
+	result := models.CertificateInfoList{}
+	if err != nil {
+		return models.CertificateInfoList{}, fmt.Errorf("fetching TUF root metadata: %w", err)
+	}
+	certificates := []string{}
+	for target, info := range targetMetadataSpecs {
+		// Only targets of type certificate
+		if strings.ToLower(info["type"]) == "fulcio" || strings.ToLower(info["type"]) == "tsa" {
+			certificates = append(certificates, target)
+			cert_content, err := s.GetTarget(ctx, tufRepoUrl, target)
+			if err != nil {
+				return models.CertificateInfoList{}, fmt.Errorf("getting target certificate content: %w", err)
+			}
+			cert_info_list, err := extractCertDetails(cert_content.Content)
+			if err != nil {
+				return models.CertificateInfoList{}, fmt.Errorf("extracting subject and issuer: %w", err)
+			}
+			for _, cert_info := range cert_info_list.Data {
+				result.Data = append(result.Data, models.CertificateInfo{
+					Issuer:     cert_info.Issuer,
+					Subject:    cert_info.Subject,
+					Expiration: cert_info.Expiration,
+					Target:     target,
+					Status:     info["status"],
+					Type:       info["type"],
+				})
+			}
+		}
+	}
+
+	return result, nil
 }
 
 // buildTufOptions returns TUF options with the provided or default repository URL.
@@ -186,8 +294,8 @@ func fetchAllTufRootMetadata(opts *tuf.Options) ([][]byte, error) {
 	return allRootBytes, nil
 }
 
-// Retrieves TUF root metadata info including version, expiration, and status.
-func extractRootMetadataInfo(rootMetadataBytes []byte) (map[string]string, error) {
+// extractRootMetadataInfo retrieves TUF root metadata info including version, expiration, and status.
+func extractRootMetadataInfo(rootMetadataBytes []byte) (models.RootMetadataInfo, error) {
 	var parsed struct {
 		Signed struct {
 			Expired string `json:"expires"`
@@ -196,12 +304,12 @@ func extractRootMetadataInfo(rootMetadataBytes []byte) (map[string]string, error
 	}
 
 	if err := json.Unmarshal(rootMetadataBytes, &parsed); err != nil {
-		return nil, fmt.Errorf("unmarshal targets metadata: %w", err)
+		return models.RootMetadataInfo{}, fmt.Errorf("unmarshal targets metadata: %w", err)
 	}
 
 	expiresTime, err := time.Parse(time.RFC3339, parsed.Signed.Expired)
 	if err != nil {
-		return nil, fmt.Errorf("parse expires time: %w", err)
+		return models.RootMetadataInfo{}, fmt.Errorf("parse expires time: %w", err)
 	}
 
 	now := time.Now().UTC()
@@ -216,11 +324,103 @@ func extractRootMetadataInfo(rootMetadataBytes []byte) (map[string]string, error
 		status = "valid"
 	}
 
-	rootMetadataInfo := map[string]string{
-		"version": strconv.Itoa(parsed.Signed.Version),
-		"expires": parsed.Signed.Expired,
-		"status":  status,
+	rootMetadataInfo := models.RootMetadataInfo{
+		Version: strconv.Itoa(parsed.Signed.Version),
+		Expires: parsed.Signed.Expired,
+		Status:  status,
 	}
 
 	return rootMetadataInfo, nil
+}
+
+// fetchTufTargetsMetadata retrieves TUF target metadata as byte slices.
+func fetchTufTargetsMetadata(opts *tuf.Options) ([]byte, error) {
+	tufCfg, err := buildTufConfig(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	up, err := updater.New(tufCfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create updater: %w", err)
+	}
+
+	if err := up.Refresh(); err != nil {
+		return nil, fmt.Errorf("failed to refresh updater: %w", err)
+	}
+
+	// Get raw targets metadata
+	targetsMeta := up.GetTrustedMetadataSet().Targets["targets"]
+	if targetsMeta == nil {
+		return nil, fmt.Errorf("targets metadata not available")
+	}
+
+	targetsBytes, err := targetsMeta.ToBytes(true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get signed root bytes: %w", err)
+	}
+
+	return targetsBytes, nil
+}
+
+// extractTargetMetadataInfo extracts status & usage of targets
+func extractTargetMetadataInfo(targetsBytes []byte) (map[string]map[string]string, error) {
+	var parsed struct {
+		Signed struct {
+			Targets map[string]struct {
+				Custom struct {
+					Sigstore struct {
+						Status string `json:"status"`
+						Usage  string `json:"usage"`
+					} `json:"sigstore"`
+				} `json:"custom"`
+			} `json:"targets"`
+		} `json:"signed"`
+	}
+
+	if err := json.Unmarshal(targetsBytes, &parsed); err != nil {
+		return nil, fmt.Errorf("unmarshal targets metadata: %w", err)
+	}
+
+	targetSpecs := make(map[string]map[string]string)
+	for name, target := range parsed.Signed.Targets {
+		targetSpecs[name] = map[string]string{
+			"status": target.Custom.Sigstore.Status,
+			"type":   target.Custom.Sigstore.Usage,
+		}
+	}
+
+	return targetSpecs, nil
+}
+
+// extractCertDetails extracts subject, issuer & status from a PEM certificate
+func extractCertDetails(certPEM string) (models.CertificateInfoList, error) {
+	var results models.CertificateInfoList
+	rest := []byte(certPEM)
+	for {
+		block, remaining := pem.Decode(rest)
+		if block == nil {
+			break
+		}
+		rest = remaining
+
+		if block.Type != "CERTIFICATE" {
+			continue
+		}
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return models.CertificateInfoList{}, fmt.Errorf("failed to parse certificate: %w", err)
+		}
+		entry := models.CertificateInfo{
+			Subject:    cert.Subject.String(),
+			Issuer:     cert.Issuer.String(),
+			Expiration: cert.NotAfter.String(),
+		}
+		results.Data = append(results.Data, entry)
+	}
+
+	if len(results.Data) == 0 {
+		return models.CertificateInfoList{}, fmt.Errorf("no valid certificates found")
+	}
+	return results, nil
 }
